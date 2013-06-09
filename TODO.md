d10e23d1a6e0428ba0a7f9b209fe29cd28ad4938
Andi Altendorfer <andi@iboard.cc>
2013-06-09 16:43:59 +0200

    Moved class SortedArray to it's own file

--- TODO.md
+++ TODO.md
@@ -4,3 +4,4 @@
 * TODO: Implement for ActiveRecord
 * TODO: Implement for MongoId
 * TODO: Written with Ruby 2.0 - make sure it works with 1.9.3 too.
+* TODO: Don't use hardcoded path /tmp for specs!
--- lib/sorted_array.rb
+++ lib/sorted_array.rb
@@ -1,71 +1,8 @@
 require 'sorted_array/version'
 require 'sorted_array/default_sorter'
+require 'sorted_array/sorted_array'
 
 # The gem/module provides a class SortedArray which keeps sorted
 # with a given Sorter-class after adding new items.
 module SortedArray
-
-  # A SortedArray takes a Sorter-object as a parameter.
-  # The array gets sorted after each push,<<,unsift.
-  # It defines marshal-methods and can be used with PStore
-  # @see DefaultSorter
-  class SortedArray < Array
-
-    # @params [Array] args the first argument is a Sorter-object
-    # @example
-    #     my_array = SortedArray.new( DefaultSorter.new(:foo) )
-    #     my_array << AnyClassWhichSupportsFoo.new(1)
-    #     my_array << AnyClassWhichSupportsFoo.new(3)
-    #     my_array << AnyClassWhichSupportsFoo.new(2)
-    #     # my_array.map(&:foo) => 1,2,3
-    # @see DefaultSorter
-    # @see Array
-    def initialize *args
-      @sorter = args.shift
-      super
-    end
-
-    # @see Array
-    def push *other
-      super
-      @sorter.sort(self)
-    end
-
-    # @see Array
-    def unshift other
-      super
-      @sorter.sort(self)
-    end
-
-    # @see Array
-    def << other
-      push *other
-      @sorter.sort(self)
-    end
-
-    protected
-
-    # @return [Array] - Sorter-object or nil, [...values...]
-    def marshal_dump
-      [@sorter ? @sorter.marshal_dump : nil, to_a]
-    end
-
-    # Initialize sorter and read in data
-    # @param [Array] array - _sorter, [..entries..]
-    def marshal_load array
-      _sorter, _data = *array
-      if _sorter
-        initialize_sorter _sorter
-        push *_data if _data
-      end
-    end
-
-    private
-
-    def initialize_sorter args
-      @sorter = args.shift.new *args
-    end
-
-  end
-
 end
